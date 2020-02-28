d0531e3bd2d7e700c09db91f3ec91d59599f9d1d
malinpho <39169948+malinpho@users.noreply.github.com>
2020-02-28 17:31:54 -0500

    card changes

--- scripts/game.js
+++ scripts/game.js
@@ -3,16 +3,18 @@ let card = document.getElementsByClassName("card");
 let cards = [...card];
 
 
+
+
 // @description toggles open and show class to display cards
 var displayCard = function (){
-    this.innerHTML="q here";
-    this.classList.toggle("open");
-    //this.classList.add("disable");
-
+    this.innerHTML="question will appear here";
+    this.classList.add("show");
 };
+
 
 // loop to add event listeners to each card
 for (var i = 0; i < cards.length; i++){
   card = cards[i];
   card.addEventListener("click", displayCard);
+
 };
